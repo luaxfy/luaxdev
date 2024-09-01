4e781eea34bb99dfa8883d249e07e52c41c081ee
luaxfy <69756282+luaxfy@users.noreply.github.com>
2024-09-02 01:23:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('socials-button').addEventListener('click', function() {
     var socials = document.getElementById('socials');
     if (socials.classList.contains('show')) {
-        socials.classList.remove('show'); // Gizle
+        socials.classList.remove('show'); 
     } else {
-        socials.classList.add('show'); // Göster
+        socials.classList.add('show'); 
     }
 });
